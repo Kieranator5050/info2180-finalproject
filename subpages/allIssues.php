e42d5425c25d8b837dc2d062177e9aeddc36d609
= <johnson.ajay1812@gmail.com>
2021-11-29 17:48:22 -0500

    values are being loaded into the table

--- subpages/allIssues.php
+++ subpages/allIssues.php
@@ -1,3 +1,5 @@
+<html>
+<link rel="stylesheet" href="home.css">
 <div class="HomeScreen">
     <div class="body">
         <div class="issues">
@@ -9,9 +11,8 @@
             <button id="filterButtons2">Open</button>
             <button id="filterButtons3">My Tickets</button>
             <br><br>
-        </div>
-        
-        <table>
+        </div>  
+        <table class="table">
             <thead>
                 <th>Title</th>
                 <th>Type</th>
@@ -19,28 +20,22 @@
                 <th>Assigned To</th>
                 <th>Created</th>
             </thead>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            <tbody>
+                <tr>
+                <?php
+                  $table= mysqli_connect("localhost","admin@project2.com","password123","bugme" );
+                  $sql = "SELECT * FROM  issues";
+                  $result= $table->query($sql);
+
+                  if ($result->num_rows>0){
+                    while ($row= $result->fetch_assoc()){
+                      echo "<tr><td>" . $row['title'] . "<td<td>" . $row['type'] . "<td><td>" . $row['status'] . "<td><td>" . $row['assigned_to'] . "<td><td>" . $row['created'] . "<tr><td>";
+                  }
+                }
+                  $table->close();
+              ?>
+            </tbody>
         </table>
     </div>
-</div>   
-<script>
-  document.getElementById("filterButtons2").addEventListener('click', all)
-
-  function all(){
-    var info= new XMLHttpRequest();
-    info.open('Get', issueView.php)
-    
-    info.onload= function(){
-      if(this.status==200){
-        var response = info.responseText
-        
-      }
-    } 
-  }
-</script>
+<div>
+</html>
