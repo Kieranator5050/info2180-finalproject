1928b8390ea2c12938f0458d574eb26fa9aa3e36
= <johnson.ajay1812@gmail.com>
2021-11-28 20:01:02 -0500

    Created table

--- subpages/allIssues.php
+++ subpages/allIssues.php
@@ -1,3 +1,46 @@
-<h1>Issues</h1>
+<div class="HomeScreen">
+    <div class="body">
+        <div class="issues">
+            <h1>Issues</h1>
+            <h1><button type="" id="filterButtons">Create New Issue</button></h1>    
+            <br><br><br><br>
+            <p>Filter by:</p>
+            <button id="filterButtons1">ALL</button>
+            <button id="filterButtons2">Open</button>
+            <button id="filterButtons3">My Tickets</button>
+            <br><br>
+        </div>
+        
+        <table>
+            <thead>
+                <th>Title</th>
+                <th>Type</th>
+                <th>Status</th>
+                <th>Assigned To</th>
+                <th>Created</th>
+            </thead>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>   
+<script>
+  document.getElementById("filterButtons2").addEventListener('click', all)
 
-<script type="text/javascript" src="./subpages/js/test.js"></script>
+  function all(){
+    var info= new XMLHttpRequest();
+    info.open('Get', issueView.php)
+    
+    info.onload= function(){
+      if(this.status==200){
+        var response = info.responseText
+        
+      }
+    } 
+  }
+</script>
