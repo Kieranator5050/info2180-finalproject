b5f36302a2a33ab32f8ada03052b8b6a13b35a24
= <johnson.ajay1812@gmail.com>
2021-11-29 23:18:23 -0500

    Added button handlers in filter.JS to filter the table using Ajax Request

--- subpages/js/filter.js
+++ subpages/js/filter.js
@@ -1,14 +1,41 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+    
+var all = document.getElementById("filterButtons1") ;
+var open = document.getElementById("filterButtons2");
+var ticket= document.getElementById("filterButtons3");
+
+all.addEventListener('click', handleAllClick);
+open.addEventListener('click',handleOpenClick);
+ticket.addEventListener('click', handleTicketClick);
+
+function handleAllClick(clickEvent){
+    var httpRequest = new XMLHttpRequest();
+    var url = 'all.php' 
+    httpRequest.onreadystatechange= sort()
+    httpRequest.open('Get', url);
+    httpRequest.send();
+}
 
 
-    document.getElementById('filterButtons1').addEventListener('click', ()=>{
-        
+function handleOpenClick(clickEvent){
+    var httpRequest = new XMLHttpRequest();
+    var url = 'open.php' 
+    httpRequest.onreadystatechange= sort()
+    httpRequest.open('Get', url);
+    httpRequest.send();
 
+}
 
-        
-    })
+function handleTicketClick(clickEvent){
 
+}
 
-
-
+function sort(){
+    if(httpRequest.readyState==XMLHttpRequest.DONE){
+        if (httpRequest.status == 200){
+            var response = httpRequest.responseText;
+            alert(response)
+        }
+    }
+}
 });
