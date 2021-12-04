ae2406d032d4493d9badac0c2062769e35e50f90
Kieranator5050 <thejugg63@gmail.com>
2021-12-04 13:54:34 -0500

    security update to adduser php file

--- scripts/createUser.php
+++ scripts/createUser.php
@@ -1,20 +1,27 @@
 <?php 
 session_start();
+if(isset($_SESSION['sessionID'])){
+    if($_SESSION['sessionID'] == "admin@project2.compassword123") {
+        require '../subpages/dbconfig.php';
 
-require '../subpages/dbconfig.php';
-
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-$fname = htmlspecialchars(filter_input(INPUT_GET,'fname',FILTER_SANITIZE_STRING));
-$lname = htmlspecialchars(filter_input(INPUT_GET,'lname',FILTER_SANITIZE_STRING));
-$email = htmlspecialchars(filter_input(INPUT_GET,'email',FILTER_SANITIZE_STRING));
-$password = htmlspecialchars(filter_input(INPUT_GET,'password',FILTER_SANITIZE_STRING));
-
-$hash_password =  password_hash($password, PASSWORD_DEFAULT);
-
-$stmt = $conn->prepare("INSERT INTO users (firstname,lastname,email,password,date_joined) VALUES (?,?,?,?,?)");
-$stmt->execute([$fname,$lname,$email,$hash_password,date("Y-m-d h:i:s")]);
-
-echo "alert('A new user was successfully created');";
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        
+        $fname = htmlspecialchars(filter_input(INPUT_GET,'fname',FILTER_SANITIZE_STRING));
+        $lname = htmlspecialchars(filter_input(INPUT_GET,'lname',FILTER_SANITIZE_STRING));
+        $email = htmlspecialchars(filter_input(INPUT_GET,'email',FILTER_SANITIZE_STRING));
+        $password = htmlspecialchars(filter_input(INPUT_GET,'password',FILTER_SANITIZE_STRING));
+        
+        $hash_password =  password_hash($password, PASSWORD_DEFAULT);
+        
+        $stmt = $conn->prepare("INSERT INTO users (firstname,lastname,email,password,date_joined) VALUES (?,?,?,?,?)");
+        $stmt->execute([$fname,$lname,$email,$hash_password,date("Y-m-d h:i:s")]);
+        
+        echo "alert('A new user was successfully created');";    
+    } else{
+        die();
+    }
+} else {
+    die();
+}
 
 ?>
